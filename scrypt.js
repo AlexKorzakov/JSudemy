7fb8adb7df697c712feb52acea084c07190cc228
Alex <example@gmail.com>
2021-06-22 13:24:27 +0300

    update if-else

--- scrypt.js
+++ scrypt.js
@@ -1,7 +1,15 @@
-const category = 'toys';
-const test = 'kokodjambo';
-console.log(`https://someurl.com/${category}/${test}`);
-console.log('Hellow world');
-const user = 'Alex';
+// const category = 'toys';
+// const test = 'kokodjambo';
+// console.log(`https://someurl.com/${category}/${test}`);
+// console.log('Hellow world');
+// const user = 'Alex';
 
-alert(`Hellow, ${user}`);
+// alert(`Hellow, ${user}`);
+
+const num = 50;
+
+if (num != 50) {
+    console.log('Error');
+} else{
+    console.log(num);
+}

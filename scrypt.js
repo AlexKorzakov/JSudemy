d6e5c13e6a2bd888824f6a512aebd5621d46674e
Alex <example@gmail.com>
2021-06-22 10:54:29 +0300

    update scrypt.js

--- scrypt.js
+++ scrypt.js
@@ -1,7 +1,7 @@
 const category = 'toys';
-
-console.log(`https://someurl.com/${category}/5`);
-
+const test = 'kokodjambo';
+console.log(`https://someurl.com/${category}/${test}`);
+console.log('Hellow world');
 const user = 'Alex';
 
 alert(`Hellow, ${user}`);
